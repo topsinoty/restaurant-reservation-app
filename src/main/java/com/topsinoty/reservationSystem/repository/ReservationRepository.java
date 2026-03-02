e127ce57b386cc7b01ad2a9421ec31911f965f73
topsinoty <topsinoty@gmail.com>
2026-03-03 00:34:00 +0200

    create service.
    Service has the getAvailableTables() for the consumers.

--- src/main/java/com/topsinoty/reservationSystem/repository/ReservationRepository.java
+++ src/main/java/com/topsinoty/reservationSystem/repository/ReservationRepository.java
@@ -3,5 +3,9 @@ package com.topsinoty.reservationSystem.repository;
 import com.topsinoty.reservationSystem.model.Reservation;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ReservationRepository extends ListCrudRepository<Reservation, Long> {
+    List<Reservation> findAllByRestaurantTableIdAndDate(Long restaurantTableId, LocalDate date);
 }
