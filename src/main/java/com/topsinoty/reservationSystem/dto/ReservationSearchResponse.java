c46eb7cae219e947687c4f72af7b27602fba5fae
topsinoty <topsinoty@gmail.com>
2026-03-03 13:38:43 +0200

    remove comment

--- src/main/java/com/topsinoty/reservationSystem/dto/ReservationSearchResponse.java
+++ src/main/java/com/topsinoty/reservationSystem/dto/ReservationSearchResponse.java
@@ -7,22 +7,3 @@ import java.util.Set;
 
 public record ReservationSearchResponse(Long id, Location location, Set<Feature> features, Integer capacity) {
 }
-
-/*
-*     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Enumerated(EnumType.STRING)
-    private Location location;
-    private int capacity;
-
-    @ElementCollection(targetClass = Feature.class)
-    @Enumerated(EnumType.STRING)
-    @CollectionTable(name = "restaurant_table_features",
-            joinColumns = @JoinColumn(name = "restaurant_table_id"))
-    @Column(name = "feature")
-    private Set<Feature> features;
-
-    @OneToMany(mappedBy = "restaurantTable")
-    private Set<Reservation> reservations;
-* */
