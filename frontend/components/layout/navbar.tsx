8f13fcb579e2afc4c719f88b1e5141efb1038931
topsinoty <topsinoty@gmail.com>
2026-03-05 11:55:19 +0200

    initial setup...need to speed up

---
+++ frontend/components/layout/navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => (
+    <nav className="w-full h-24 sticky top-0 bg-primary">
+        <div className="px-8 flex items-center h-full">
+            <p className={"text-3xl text-primary-foreground font-bold"}>Topsinoty Resto</p>
+        </div>
+    </nav>
+)
