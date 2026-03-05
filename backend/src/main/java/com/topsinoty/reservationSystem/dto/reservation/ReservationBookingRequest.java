891618ca0cb3e4e7d6992266e725429926f32422
topsinoty <topsinoty@gmail.com>
2026-03-05 12:34:17 +0200

    move to package

--- backend/src/main/java/com/topsinoty/reservationSystem/dto/reservation/ReservationBookingRequest.java
+++ backend/src/main/java/com/topsinoty/reservationSystem/dto/reservation/ReservationBookingRequest.java
@@ -1,4 +1,4 @@
-package com.topsinoty.reservationSystem.dto;
+package com.topsinoty.reservationSystem.dto.reservation;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
