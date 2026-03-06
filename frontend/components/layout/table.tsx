092e3bfdeee2a950d4a0906f170fcd126dafefaf
topsinoty <topsinoty@gmail.com>
2026-03-06 16:27:37 +0200

    add window to design

--- frontend/components/layout/table.tsx
+++ frontend/components/layout/table.tsx
@@ -13,6 +13,13 @@ export function Table({
 	const ratio = capacity / 10;
 	const size = cellSize * (0.4 + ratio * 0.6);
 
+	const hasWindow = features.includes("WINDOW_SIDE");
+
+	const windowLocation: WindowLocation = {
+		left: hasWindow && x === 0,
+		top: hasWindow && y === 0,
+	};
+
 	return (
 		<div
 			className="absolute border rounded-xl flex items-center justify-center bg-white text-xs"
@@ -28,5 +35,12 @@ export function Table({
 			<br />
 			{capacity}
 		</div>
+			{windowLocation.left && capacity > 3 && (
+				<div className="absolute left-0 top-1/2 -translate-y-1/2 w-1 h-3/5 bg-black/40 rounded-r-full" />
+			)}
+
+			{windowLocation.top && capacity > 3 && (
+				<div className="absolute top-0 left-1/2 -translate-x-1/2 h-1 w-3/5 bg-black" />
+			)}
 	);
 }
