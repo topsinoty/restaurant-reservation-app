a225b5b73a9ec93320d4138b348cca6d1d9ed312
topsinoty <topsinoty@gmail.com>
2026-03-11 12:17:45 +0200

    make it handle api errors only

--- backend/src/main/java/com/topsinoty/reservationSystem/config/GlobalApiExceptionHandler.java
+++ backend/src/main/java/com/topsinoty/reservationSystem/config/GlobalApiExceptionHandler.java
@@ -1,26 +1,19 @@
 package com.topsinoty.reservationSystem.config;
 
 import com.topsinoty.reservationSystem.dto.ApiResult;
+import com.topsinoty.reservationSystem.exception.ApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ApiResult<Void>> handleNotFound(NoSuchElementException ex) {
+public class GlobalApiExceptionHandler {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiResult<Void>> handleNotFound(ApiException ex) {
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ApiResult.failure(ex.getMessage()));
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ApiResult<Void>> handleConflict(IllegalStateException ex) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
+                .status(ex.status())
                 .body(ApiResult.failure(ex.getMessage()));
     }
 
