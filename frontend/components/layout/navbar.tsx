9075ac442dce87cf68fcefd65ca3c0d98de30f69
topsinoty <topsinoty@gmail.com>
2026-03-12 14:48:07 +0200

    change the ui for bigger font

--- frontend/components/layout/navbar.tsx
+++ frontend/components/layout/navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => (
 	<nav className="sticky top-0 z-40 w-full border-b bg-white/90 backdrop-blur">
-		<div className="container mx-auto flex h-16 items-center px-4">
-			<p className="text-xl font-semibold tracking-tight text-slate-800 sm:text-2xl">
+		<div className="container mx-auto flex h-min py-4 lg:py-8 items-center px-4">
+			<p className="text-xl font-semibold tracking-tight text-slate-800 sm:text-2xl lg:text-3xl">
 				Topsinoty Resto
 			</p>
 		</div>
