aee05a97ae413da4537b62414df79fbf0519e386
topsinoty <topsinoty@gmail.com>
2026-04-15 21:55:02 +0300

    add support for 404 errors

--- backend/src/main/java/com/topsinoty/reservationSystem/config/GlobalApiExceptionHandler.java
+++ backend/src/main/java/com/topsinoty/reservationSystem/config/GlobalApiExceptionHandler.java
@@ -4,12 +4,14 @@ import com.topsinoty.reservationSystem.dto.ApiResult;
 import com.topsinoty.reservationSystem.exception.ApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.stream.Collectors;
 
@@ -41,7 +43,7 @@ public class GlobalApiExceptionHandler {
                 .body(ApiResult.failure(message));
     }
 
-    // todo exception handler for wrong route and wrong method use...
+    // todo exception handler for wrong method use...
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiResult<Void>> handleArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
@@ -58,6 +60,15 @@ public class GlobalApiExceptionHandler {
                 .badRequest()
                 .body(ApiResult.failure("Malformed JSON request"));
     }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ApiResult<Void>> handleMissingHandler(NoHandlerFoundException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResult.failure(ex.getMessage()));
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResult<Void>> handleGeneric(Exception ex) {
